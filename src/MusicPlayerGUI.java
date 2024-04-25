f2916d03a997b320d68f8492b7c28b1a8100b662
NOOYNOOT <jonas070105@gmail.com>
2024-04-25 09:21:26 +0200

    Basic start of the GUI

---
+++ src/MusicPlayerGUI.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class MusicPlayerGUI extends JFrame {
+    public MusicPlayerGUI(){
+        // calls JFrame constructor to configure gui and set header title to "Music Player"
+    super("Music Player");
+
+    // set width and height
+    setSize(400, 600);
+
+    // end process when app is closed
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    // launch the app at the center of the screen
+    setLocationRelativeTo(null);
+
+    // prevent the app from being resized
+    setResizable(false);
+
+    // set layout to null witch will allow us to control the (x, y) coordinates of our components
+    // and also set the height and width
+    setLayout(null);
+
+
+    }
+}
