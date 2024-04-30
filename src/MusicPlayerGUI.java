9d538304fa2f164bf482b421c9b320ccbe0a4c1c
NOOYNOOT <jonas070105@gmail.com>
2024-04-30 08:23:11 +0200

    added JBarMenu with load song and load/create playlist

--- src/MusicPlayerGUI.java
+++ src/MusicPlayerGUI.java
@@ -32,6 +32,33 @@ public class MusicPlayerGUI extends JFrame {
     private void addToolbar(){
         JToolBar toolBar = new JToolBar();
         toolBar.setBounds(0, 0, getWidth(), 20);
+
+        // prevent toolbar from being moved
+        toolBar.setFloatable(false);
+
+        // add drop down menu
+        JMenuBar menuBar = new JMenuBar();
+        toolBar.add(menuBar);
+
+        // add a song menu where the loading song option will be
+        JMenu songMenu = new JMenu("Song");
+        menuBar.add(songMenu);
+
+        // adding the "load song" item in the songMenu
+        JMenuItem loadSong = new JMenuItem("Load Song");
+        songMenu.add(loadSong);
+
+        // here will we add the playlist menu
+        JMenu playlist = new JMenu("Playlist");
+        menuBar.add(playlist);
+
+        // new we will add the create the items to the playlist menu
+        JMenuItem createPlaylist = new JMenuItem("Create Playlist");
+        playlist.add(createPlaylist);
+
+        JMenuItem loadPlaylist = new JMenuItem("Load Playlist");
+        playlist.add(loadPlaylist);
+
         add(toolBar);
     }
 }
